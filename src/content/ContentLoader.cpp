4df4209bc59944e97102be7a618292378dc301a4
MihailRis <mexaruc@gmail.com>
2025-12-02 18:21:52 +0300

    fix generated item's script

--- src/content/ContentLoader.cpp
+++ src/content/ContentLoader.cpp
@@ -289,6 +289,9 @@ void ContentLoader::loadContent(const dv::value& root) {
             item.icon = def.name;
             item.placingBlock = def.name;
             item.tags = def.tags;
+            item.scriptFile = def.name + BLOCK_ITEM_SUFFIX + ".lua";
+
+            logger.info() << "scriptFile: " << item.scriptFile;
     
             for (uint j = 0; j < 4; j++) {
                 item.emission[j] = def.emission[j];
