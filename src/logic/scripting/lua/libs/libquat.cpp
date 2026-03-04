563b210500d16e7996acf6e39f6c64ca392cbbfb
RomanDonw <romandonw628@gmail.com>
2026-03-04 19:45:21 +1000

    replaced radians on degrees

--- src/logic/scripting/lua/libs/libquat.cpp
+++ src/logic/scripting/lua/libs/libquat.cpp
@@ -46,7 +46,7 @@ static int l_tostring(lua::State* L) {
 
 static int l_from_euler(lua::State *L) {
     uint argc = lua::check_argc(L, 1, 2);
-    glm::vec3 euler = lua::tovec3(L, 1);
+    glm::vec3 euler = glm::radians(lua::tovec3(L, 1));
 
     if (argc == 2) return lua::setquat(L, 2, glm::quat(euler));
     return lua::pushquat(L, glm::quat(euler));
