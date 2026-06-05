d2f272e403cf000810396e4760e9ee39c6e8b8b3
MihailRis <mexaruc@gmail.com>
2026-06-05 23:22:44 +0300

    add table.keys(t)

--- res/modules/internal/extensions/table.lua
+++ res/modules/internal/extensions/table.lua
@@ -201,3 +201,11 @@ function table.tostring(t)
     end
     return s..']'
 end
+
+function table.keys(t)
+    local keys = {}
+    for key, _ in pairs(t) do
+        table.insert(keys, key)
+    end
+    return keys
+end
