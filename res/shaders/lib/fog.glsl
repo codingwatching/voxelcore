86096553ce847768cc1b17ebf7b27f15a76ecb25
MihailRis <mexaruc@gmail.com>
2026-07-18 16:20:10 +0300

    fix

--- res/shaders/lib/fog.glsl
+++ res/shaders/lib/fog.glsl
@@ -7,13 +7,17 @@ uniform float u_weatherFogOpacity;
 uniform float u_weatherFogDencity;
 uniform float u_weatherFogCurve;
 
-float calc_fog(float depth) {
+float calc_fog(float depth, float fogFactor, float fogCurve) {
     return min(
         1.0,
-        max(pow(depth * u_fogFactor, u_fogCurve),
+        max(pow(depth * u_fogFactor * fogFactor, u_fogCurve * fogCurve),
             min(pow(depth * u_weatherFogDencity, u_weatherFogCurve),
                 u_weatherFogOpacity))
     );
+}
+
+float calc_fog(float depth) {
+    return calc_fog(depth, 1.0, 1.0);
 }
 
 #endif // FOG_GLSL_
