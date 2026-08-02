2eae8f6f275e7a41ac30309e61f8d22d8a644ac7
EsPaKira <kirillplay1361@gmail.com>
2026-08-02 19:18:37 +0400

    fix block.index() calls in grass_block.lua and export base:util.calc_loot()

--- res/content/base/modules/util.lua
+++ res/content/base/modules/util.lua
@@ -12,14 +12,14 @@ function util.drop(ppos, itemid, count, data, pickup_delay)
     }})
 end
 
-local function calc_loot(loot_table)
+function calc_loot(loot_table)
     local results = {}
     for _, loot in ipairs(loot_table) do
         local chance = loot.chance or 1
         local count = loot.count or 1
-        
+
         local roll = math.random()
-        
+
         if roll < chance then
             if loot.min and loot.max then
                 count = math.random(loot.min, loot.max)
